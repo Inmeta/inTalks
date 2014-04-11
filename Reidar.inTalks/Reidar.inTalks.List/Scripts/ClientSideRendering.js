978763a9426daee5498b7575ef8788733f64f53a
Reidar Husmo <Reidar.Husmo@inmeta.com>
2014-04-11 09:39:22 +0200

    More test data, working client side rendering

--- Reidar.inTalks/Reidar.inTalks.List/Scripts/ClientSideRendering.js
+++ Reidar.inTalks/Reidar.inTalks.List/Scripts/ClientSideRendering.js
@@ -44,11 +44,24 @@
     //};
     oc.Templates = {};
     oc.Templates.Fields = {
-        "Away": { "View": getDemoHtml }
-        //"Title": { "View": getDemoHtml }
+        "Away": { "View": getPrettyCountryHtml },
+        "Home": { "View": getPrettyCountryHtml }
+    //"Title": { "View": getDemoHtml }
 };
     SPClientTemplates.TemplateManager.RegisterTemplateOverrides(oc);
 })();
+
+function getPrettyCountryHtml(ctx) {
+    var fieldValue = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+    if (fieldValue === "Portugal")
+    {
+        return "<span style='color: red'>" + fieldValue + "</span>";
+    }
+    if (fieldValue === "Spain") {
+        return "<span style='color: blue'>" + fieldValue + "</span>";
+    }
+    return fieldValue;
+}
 
 function getDemoHtml(ctx) {
     //var home = ctx.CurrentItem.Title;
