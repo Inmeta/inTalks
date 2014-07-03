bc89c28f3afa4c636d5ef20b0e8c64f1c817d241
Reidar Husmo <Reidar.Husmo@inmeta.com>
2014-07-03 15:10:38 +0200

    REST works.

--- Reidar.inTalks/Inmeta.SP.IBC/Pages/IBC.aspx
+++ Reidar.inTalks/Inmeta.SP.IBC/Pages/IBC.aspx
@@ -5,14 +5,46 @@
 
 <asp:Content ContentPlaceHolderId="PlaceHolderAdditionalPageHead" runat="server">
     <SharePoint:ScriptLink name="sp.js" runat="server" OnDemand="true" LoadAfterUI="true" Localizable="false" />
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
     <style type="text/css">
+        #contents {
+            background-color: #111;
+        }
         #s4-ribbonrow {
             display: none;
         }
+        h1 {
+            font-family: "Josefin Sans", "Segoe UI Light", "Gill Sans", 'Lucida Bright', sans-serif;
+            font-size: 12vmin;
+            font-size: 10vm;
+            color: #aaa;
+        }
+        p {
+            font-family: "Josefin Sans", "Segoe UI Light", "Gill Sans", 'Lucida Bright', sans-serif;
+            font-size: 3vmin;
+            font-size: 3vm;
+            color: #aaa;
+        }
+        #contents div {
+            font-family: "Segoe UI Light", "Josefin Sans", "Gill Sans", 'Lucida Bright', sans-serif;
+            font-size: 3vmin;
+            font-size: 3vm;
+            color: #aaa;
+        }
+
     </style>
+    <script type="text/javascript" src="../Scripts/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="/_layouts/15/sp.runtime.js"></script>
+    <script type="text/javascript" src="/_layouts/15/sp.js"></script>
+    <script type="text/javascript" src="../Scripts/App.js"></script>
 </asp:Content>
 
 <asp:Content ContentPlaceHolderId="PlaceHolderMain" runat="server">
-    <WebPartPages:WebPartZone runat="server" FrameType="TitleBarOnly" ID="full" Title="loc:full" Visible="false" />
-    Content goes here.
+    <div style="background-color:#111">
+        <WebPartPages:WebPartZone runat="server" FrameType="TitleBarOnly" ID="full" Title="loc:full" Visible="false" />
+        <div id="contents"></div>
+        <h1>Inmarsat</h1>
+        <p>Inmarsat er et prosjekt. Ålesund. Intranett og ekstranett.</p>
+                <input type="button" value="dirr" onclick="inmeta.ibc.functions.fillInformation($('#contents'));" />
+    </div>
 </asp:Content>
--- Reidar.inTalks/Inmeta.SP.IBC/Scripts/App.js
+++ Reidar.inTalks/Inmeta.SP.IBC/Scripts/App.js
@@ -24,3 +24,80 @@ function onGetUserNameSuccess() {
 function onGetUserNameFail(sender, args) {
     alert('Failed to get user name. Error:' + args.get_message());
 }
+
+var inmeta = window.inmeta || {};
+
+if (!inmeta["ibc"]) {
+    inmeta.ibc = {};
+}
+inmeta.ibc.functions = (function () {
+    'use strict';
+    function fillInformation(contentsElement) {
+        readProjects();
+        //var projects = getInformation();
+        //var outerDiv = $("<div>");
+        //for (var i = 0; i < projects.length; i++) {
+        //    var title = projects[i].title;
+        //    var body = projects[i].body;
+        //    var team = projects[i].team;
+        //    $("<div>").html("<h1>" + title + "</h1><p>" + body + "</p>").appendTo(outerDiv);
+        //}
+        //outerDiv.appendTo(contentsElement);
+    }
+    function readProjects() {
+        // clear resultsArea and add spinning gears icon
+        $("#contents").empty();
+        $("<img>", { "src": "/_layouts/images/GEARS_AN.GIF" }).appendTo("#contents");
+
+        // begin work to call across network
+        var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/Web/Lists/getByTitle('Broadcast items')/items";
+
+        // execute AJAX request 
+        var requestHeaders = {
+            "accept": "application/json;odata=verbose"
+        }
+        $.ajax({
+            url: requestUri,
+            headers: requestHeaders,
+            success: foundProjects,
+            error: onError
+        });
+    }
+    function onError(err) {
+        $("#contents").text("ERROR: " + JSON.stringify(err));
+    }
+    function foundProjects(data) {
+        $("#contents").empty();
+
+        var projects = /* JSON.*/ data.d.results;
+
+        var outerDiv = $("<div>");
+        for (var i = 0; i < projects.length; i++) {
+            var title = projects[i].Title;
+            var body = projects[i].Body;
+            var team = projects[i].TeamMembers;
+            var bodyEl = $("<p>").html(body);
+            console.log(bodyEl.html());
+            $("<div>").html("<h1>" + title + "</h1><p>" + body + "</p>").appendTo(outerDiv);
+        }
+        outerDiv.appendTo($("#contents"));
+    }
+    function getInformation() {
+        var info = [];
+        var team = [];
+        team[0] = "Pål";
+        team[1] = "Reidar";
+        team[2] = "Mario";
+        team[3] = "Anders";
+        team[4] = "Jon";
+        team[5] = "Knut";
+        var team2 = []; team2[0] = "Svarteper";
+        info[0] = { title: "Inmarsat", body: "is a project", team: team };
+        info[1] = { title: "Espire", body: "First rule of Espire: Nobody talks about Espire", team: team2 };
+        return info;
+    }
+    return {
+        getInformation: getInformation,
+        fillInformation: fillInformation
+    }
+})();
